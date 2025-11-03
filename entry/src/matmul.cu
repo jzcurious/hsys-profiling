212b1f5c47954a87c6dc0798cab33c306ace1b83
jzcurious <paranoid.hpc.io@gmail.com>
2025-11-04 01:01:21 +0700

    add profiling case

--- entry/src/matmul.cu
+++ entry/src/matmul.cu
@@ -32,12 +32,15 @@ struct MatmulPipeline {
 };
 
 int main() {
-  MatmulPipeline matmul{1024};
+  MatmulPipeline matmul1024{1024};
+  matmul1024.run(hsys::matmul<>);
+  matmul1024.run(hsys::matmul_extend_bright<>);
+  matmul1024.run(hsys::matmul_extend_adown<>);
+  matmul1024.run(hsys::matmul_extend_sym2<>);
+  matmul1024.run(hsys::matmul_extend_sym3<>);
+  matmul1024.run(hsys::matmul_extend_symn<>);
 
-  matmul.run(hsys::matmul<>);
-  matmul.run(hsys::matmul_extend_bright<>);
-  matmul.run(hsys::matmul_extend_adown<>);
-  matmul.run(hsys::matmul_extend_sym2<>);
-  matmul.run(hsys::matmul_extend_sym3<>);
-  matmul.run(hsys::matmul_extend_symn<>);
+  MatmulPipeline matmul64{64};
+  matmul64.run(hsys::matmul_extend_symn<>);
+  matmul64.run(hsys::matmul_extend_symn<8, 10>);
 }
