2b62b953823678cffb0f2b589756c7f6aea18fad
jzcurious <paranoid.hpc.io@gmail.com>
2025-11-10 07:15:39 +0700

    fix vadd

--- vadd/include/vadd/context.cuh
+++ vadd/include/vadd/context.cuh
@@ -35,7 +35,7 @@ struct Context {
   template <ViewK... T>
   __host__ void set_args(const T&... new_arg) {
     auto new_args = std::make_tuple(new_arg...);
-    cudaMemcpy(args_, &new_args, sizeof(new_args), cudaMemcpyHostToDevice);
+    cudaMemcpyAsync(args_, &new_args, sizeof(new_args), cudaMemcpyHostToDevice, stream_);
     slot_->set_args(args_);
   }
 
--- vadd/include/vadd/vadd.cuh
+++ vadd/include/vadd/vadd.cuh
@@ -8,7 +8,7 @@ namespace hsys::kernels {
 __global__ void vadd(
     VectorViewK auto c, const VectorViewK auto a, const VectorViewK auto b) {
   auto tid = blockDim.x * blockIdx.x + threadIdx.x;
-  c[tid] += a[tid] + b[tid];
+  c[tid] = a[tid] + b[tid];
 }
 
 }  // namespace hsys::kernels
@@ -18,7 +18,7 @@ namespace hsys {
 template <VectorK VectorT = hsys::Vector<float>>
 void vadd(VectorT& c, const VectorT& a, const VectorT& b) {
   constexpr unsigned block = 128;
-  const unsigned grid = std::ceil(a.size() / block);
+  const unsigned grid = std::ceil(static_cast<float>(a.size()) / block);
   hsys::kernels::vadd<<<grid, block>>>(c.view(), a.view(), b.view());
 }
 
