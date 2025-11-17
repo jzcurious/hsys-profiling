c5a77ca73ac6d0aac01c21722b9f5bbedba7676d
jzcurious <paranoid.hpc.io@gmail.com>
2025-11-17 15:45:50 +0700

    add TaskBuffer

--- pack/include/pack/buffer.cuh
+++ pack/include/pack/buffer.cuh
@@ -5,21 +5,25 @@
 namespace hsys {
 
 template <DynPackKind DynPackT, class CallbackT>
+// requires std::is_invocable_v<CallbackT, DynPackT>
 struct TaskBuffer {
+  using common_task_t = typename DynPackT::common_task_t;
 
-  using commom_task_t = typename DynPackT::common_task_t;
-
-  TaskBuffer(DynPackT&& pack, CallbackT&& callback)
+  TaskBuffer(DynPackT&& pack, const CallbackT& callback)
       : pack_(std::move(pack))
       , callback_(std::move(callback)) {}
 
+  void flush() {
+    callback_(pack_);
+  }
+
   void push(const common_task_t& task) {
-    if (pack_.is_full()) callback_(pack_) else pack_.push(task);
+    if (pack_.is_full()) flush();
+    pack_.push(task);
   }
 
  private:
-  DynPack pack_ {}
-
+  DynPackT pack_{};
   CallbackT callback_;
 };
 
